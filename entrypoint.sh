f03a7ea585b330396e0c03724e873255b9ab44a8
Gabriele Toselli <toselli.gabriele@gmail.com>
2021-09-03 15:17:04 +0200

    Test: demo action

---
+++ entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh -l
+
+
+INPUT_DEPLOYMENT_NAME=${INPUT_DEPLOYMENT_NAME##*/}
+
+echo -n $GCLOUD_SERVICE_ACCOUNT_KEYFILE > ./gcloud-api-key.json
+gcloud auth activate-service-account --key-file gcloud-api-key.json
+gcloud config set project $GCLOUD_PROJECT   
+
+gcloud container clusters get-credentials $INPUT_CLUSTER_NAME --zone=$INPUT_CLUSTER_ZONE
+kubectl set image deployment $INPUT_DEPLOYMENT_NAME $INPUT_DEPLOYMENT_NAME=$INPUT_IMAGE_PATH --namespace=$INPUT_DEPLOYMENT_NAMESPACE

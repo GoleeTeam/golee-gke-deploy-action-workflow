8729afaaa1721392549ebe889311c0c30e683260
robygolee <roberto.colombo@golee.it>
2022-12-14 15:08:05 +0100

    tentative(gke-auth): 3

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,7 +11,7 @@ gcloud config set project $GCLOUD_PROJECT
 
 echo install gke-gcloud-auth-plugin
 
-gcloud components install gke-gcloud-auth-plugin
+sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
 
 gcloud container clusters get-credentials $INPUT_CLUSTER_NAME --zone=$INPUT_CLUSTER_ZONE
 kubectl set image deployment $INPUT_DEPLOYMENT_NAME $INPUT_DEPLOYMENT_NAME=$INPUT_IMAGE_PATH --namespace=$INPUT_DEPLOYMENT_NAMESPACE

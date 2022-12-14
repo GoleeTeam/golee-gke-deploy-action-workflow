4c5139817fcc57e2f7fea8441d547035a4072ba0
Luca Giovenzana <luca@giovenzana.org>
2022-12-14 23:19:12 +0100

    fix(entrypoint): gke gcloud auto plugin

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,11 @@
 #!/bin/sh -l
 
-
-echo estart entrypoint.sh
-
 INPUT_DEPLOYMENT_NAME=${INPUT_DEPLOYMENT_NAME##*/}
 
+export USE_GKE_GCLOUD_AUTH_PLUGIN=Trueß
 echo -n $GCLOUD_SERVICE_ACCOUNT_KEYFILE > ./gcloud-api-key.json
 gcloud auth activate-service-account --key-file gcloud-api-key.json
-gcloud config set project $GCLOUD_PROJECT
-
-echo install gke-gcloud-auth-plugin
-
-sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin
+gcloud config set project $GCLOUD_PROJECT   
 
 gcloud container clusters get-credentials $INPUT_CLUSTER_NAME --zone=$INPUT_CLUSTER_ZONE
 kubectl set image deployment $INPUT_DEPLOYMENT_NAME $INPUT_DEPLOYMENT_NAME=$INPUT_IMAGE_PATH --namespace=$INPUT_DEPLOYMENT_NAMESPACE

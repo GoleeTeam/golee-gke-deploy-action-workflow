87945382195fb64bb9f12865f73c7dbc4ebf68b6
dodo <damiandominella@gmail.com>
2022-12-15 17:36:11 +0100

    fix(): removed typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 
 INPUT_DEPLOYMENT_NAME=${INPUT_DEPLOYMENT_NAME##*/}
 
-export USE_GKE_GCLOUD_AUTH_PLUGIN=Trueß
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
 echo -n $GCLOUD_SERVICE_ACCOUNT_KEYFILE > ./gcloud-api-key.json
 gcloud auth activate-service-account --key-file gcloud-api-key.json
 gcloud config set project $GCLOUD_PROJECT   
